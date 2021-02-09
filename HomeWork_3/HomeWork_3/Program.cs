80632ed29414c5439672e7e42b1ab3f11d66645b
VavriichukDima <78602077+VavriichukDima@users.noreply.github.com>
2021-02-09 20:37:18 +0200

    HomeWork_3
    Создать массив на N элементов, где N указывается с консольной строки. Заполнить его случайными числами от 1 до 26 включительно. Создать 2 массива, где в 1 массиве будут значение только четных значений, а во втором нечетных. Заменить числа в 1 и 2 массиве  на буквы английского алфавита. Значения ячеек этих массивов равны порядковому номеру каждой буквы в алфавите. Если же буква является одной из списка (a, e, i, d, h, j) то она должна быть в верхнем регистре. Вывести на экран результат того, в каком из массивов будет больше букв в верхнем регистре.  Вывести оба массива на экран. Каждый из массивов должен быть выведен 1 строкой, где его значения будут разделены пробелом

---
+++ HomeWork_3/HomeWork_3/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace HomeWork_3
+{
+    using System;
+
+    /// <summary>
+    /// a.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// a.
+        /// </summary>
+        /// <param name="args">project.</param>
+        public static void Main(string[] args)
+        {
+            Console.Write("New random array:");
+            string tmp = Console.ReadLine();
+            int n = Convert.ToInt32(tmp);
+            int[] arr = new int[n];
+            int evenCount = 0;
+            int oddCount = 0;
+            char[] evenArr = new char[n];
+            char[] oddArr = new char[n];
+            char[] letterArr = new char[26] { 'A', 'b', 'c', 'D', 'E', 'f', 'g', 'H', 'I', 'J', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = new Random().Next(1, 26);
+                Console.Write($"{arr[i]} ");
+            }
+
+            Console.WriteLine();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if ((arr[i] % 2) == 0)
+                {
+                    evenArr[i] = letterArr[arr[i] - 1];
+                    oddArr[i] = '0';
+                    if (evenArr[i] >= 'A')
+                    {
+                        evenCount++;
+                    }
+                }
+                else
+                {
+                    evenArr[i] = '0';
+                    oddArr[i] = letterArr[arr[i] - 1];
+                    if (oddArr[i] >= 'A')
+                    {
+                        oddCount++;
+                    }
+                }
+            }
+
+            Console.WriteLine();
+            if (evenCount > oddCount)
+            {
+                Console.WriteLine("Even array have more title letters than odd array");
+            }
+            else
+            {
+                Console.WriteLine("Odd array have more title letters than even array");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("EvenArr:");
+            for (int i = 0; i < evenArr.Length; i++)
+            {
+                Console.Write($"{evenArr[i]} ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("OddArray: ");
+            for (int i = 0; i < oddArr.Length; i++)
+            {
+               Console.Write($"{oddArr[i]} ");
+            }
+        }
+    }
+}
